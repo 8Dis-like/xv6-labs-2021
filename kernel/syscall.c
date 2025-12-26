5666b6e73d1ceb298d86d7d1edcf670e872ec7d4
Zugho <Zugho@gmail.com>
2025-12-26 10:30:52 -0800

    formatting for grades

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -167,7 +167,7 @@ syscall(void)
     p->trapframe->a0 = syscalls[num]();
 
     if((p->syscall_trace>>num)&1){
-      printf("%d:syscall %s->%d\n",p->pid,syscall_names[num],p->trapframe->a0);
+      printf("%d: syscall %s -> %d\n",p->pid,syscall_names[num],p->trapframe->a0);
     }
   } else {
     printf("%d %s: unknown sys call %d\n",
